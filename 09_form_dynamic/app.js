9a4e1543f26fe74b8a8f9c9f464cb26fcd3032e8
algemorge <mafia18@naver.com>
2023-03-30 18:41:49 +0900

    study : ajax 동적 페이지 구현

--- 09_form_dynamic/app.js
+++ 09_form_dynamic/app.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8000;
 
 app.set("view engine", "ejs"); // view 엔진 등록
 app.use("/views", express.static(__dirname + "/views")) // ejs를 담을 views 폴더 경로 설정
 app.use(express.urlencoded({ extended: true })) // post 요청으로 들어오는 모든 형식의 데이터를 파싱(분석
 app.use(express.json()); //json 형태로 데이터를 주고 받음
 
+// Routing (주소설정)
 app.get('/', (req, res) => {
-   res.render('index');
+   res.render('dynamic');
 })
 
+// 1-1. /ajax get 요청 : query 
+app.get('/ajax', function (req, res) {
+   console.log(req.query);
+   res.send(req.query)
+})
+
+// 1-2 /ajax post 요청 : body
+app.post('/ajax', function (req, res) {
+   console.log(req.body);
+   res.send(req.body)
+})
 
 app.listen(PORT, function () {
    console.log(`[${PORT}] 포트가 실행되었습니다.`);
-   // console.log(`http://localhost:${PORT}`);
 })
 
