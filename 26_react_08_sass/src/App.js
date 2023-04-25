c1d128128846faf3b28b789dd3dc1f3e555e7ef4
algemorge <mafia18@naver.com>
2023-04-25 19:33:08 +0900

    stydy : scss basic

---
+++ 26_react_08_sass/src/App.js
@@ -0,0 +1,15 @@
+import CssComponent from "./CssComponent";
+import Prac from "./Prac";
+import SassComponent from "./SassComponent";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <CssComponent /> */}
+      {/* <SassComponent /> */}
+      <Prac />
+    </div>
+  );
+}
+
+export default App;
---
+++ 26_react_08_sass/src/Prac.js
@@ -0,0 +1,26 @@
+import './style/Prac.scss'
+const Prac = () => {
+   return (
+      // <div className="larva">
+      //    <div className="body body1">'
+      //       <div className="eye eye-white">
+      //          <div className="eye eye-black"></div>
+      //       </div>
+      //    </div>
+      //    <div className="body body2"></div>
+      //    <div className="body body3"></div>
+      //    <div className="body body4"></div>
+      //    <div className="body body5"></div>
+      //    <div className="body body6"></div>
+      // </div>
+
+      // prac2
+      <div className="circle">
+         <div className="circle pink"></div>
+         <div className="circle yellow"></div>
+         <div className="circle green"></div>
+      </div>
+   )
+}
+
+export default Prac;
